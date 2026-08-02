683d961a744c6b82a0fdbe96dc82b6dadc8d24b1
vinay agrawal <vinayagarwal5@gmail.com>
2026-08-02 10:25:53 +0530

    added additional constants

--- python/utils/constants.py
+++ python/utils/constants.py
@@ -9,3 +9,21 @@ PAYMENT_MODE_CC = "Credit Card"
 PHONEPE_DATE_FORMAT = "%b %d, %Y"
 
 ICICI_DATE_FORMAT = "%d-%b-%y"
+
+# ==========================================================
+# Transaction Types
+# ==========================================================
+
+TRANSACTION_DEBIT = "Debit"
+TRANSACTION_CREDIT = "Credit"
+
+TRANSACTION_EXPENSE = "Expense"
+TRANSACTION_INCOME = "Income"
+
+# ==========================================================
+# Budget Status
+# ==========================================================
+
+STATUS_WITHIN_BUDGET = "🟢 Within Budget"
+STATUS_NEAR_LIMIT = "🟡 Near Limit"
+STATUS_OVER_BUDGET = "🔴 Over Budget"
